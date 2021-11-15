102767969a33b5a78bd30f285b5844fd2c900871
Francisco Ricardo <78240032+franric@users.noreply.github.com>
2021-11-14 23:49:07 -0400

    Lista de Funcionários

---
+++ foxcon/Controllers/FuncionarioController.cs
@@ -0,0 +1,21 @@
+﻿using foxcon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace foxcon.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        // GET: Funcionario
+        public ActionResult Index()
+        {
+            using(var dbFun = new foxEntitSql())
+            {
+                return View(dbFun.Employees.ToList());
+            }            
+        }
+    }
+}
---
+++ foxcon/Models/FuncionarioModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace foxcon.Models
+{
+    public class FuncionarioModel
+    {
+        public int id_departamento { get; set; }
+        [Required]
+        [Display(Name = "Nome do Empresgado")]
+        public string name { get; set; }
+        [Required]
+        [Display(Name = "Salário")]
+        public decimal salary { get; set; }
+        [Display(Name = "Genero")]
+        public string gender { get; set; }
+        [Display(Name = "Genero")]
+        public string active { get; set; }
+    }
+    
+    [MetadataType(typeof(FuncionarioModel))]
+    public partial class Employees
+    {
+
+    }
+}

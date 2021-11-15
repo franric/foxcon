e8ce8d44a4fbdb767d8cf85a106535fc7f08cc42
Francisco Ricardo <78240032+franric@users.noreply.github.com>
2021-11-14 20:55:07 -0400

    Modificação da Master Page

--- foxcon/App_Start/BundleConfig.cs
+++ foxcon/App_Start/BundleConfig.cs
@@ -8,6 +8,24 @@ namespace foxcon
         // Para obter mais informações sobre o agrupamento, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/Content/js").Include(
+                       "~/Content/lte/plugins/jquery/jquery.min.js",
+                       "~/Content/lte/plugins/bootstrap/js/bootstrap.bundle.min.js",
+                       "~/Content/lte/dist/js/adminlte.min.js",
+                       "~/Content/lte/dist/js/adminlte.min.js",
+                       "~/Content/lte/dist/js/demo.js"));
+
+            bundles.Add(new ScriptBundle("~/Content/datatable").Include(
+                       "~/Content/lte/plugins/datatables/jquery.dataTables.min.js",
+                       "~/Content/lte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js",
+                       "~/Content/lte/plugins/datatables-responsive/js/dataTables.responsive.min.js",
+                       "~/Content/lte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js",
+                       "~/Content/lte/plugins/datatables-buttons/js/dataTables.buttons.min.js",
+                       "~/Content/lte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js",
+                       "~/Content/lte/plugins/datatables-buttons/js/buttons.html5.min.js",
+                       "~/Content/lte/plugins/datatables-buttons/js/buttons.print.min.js",
+                       "~/Content/lte/plugins/datatables-buttons/js/buttons.colVis.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -19,12 +37,14 @@ namespace foxcon
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/lte/plugins/fontawesome-free/css/all.min.css",
+                      "~/Content/lte/dist/css/adminlte.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css/datatable").Include(
+                      "~/Content/lte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css",
+                      "~/Content/lte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css",
+                      "~/Content/lte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css"));
         }
     }
 }

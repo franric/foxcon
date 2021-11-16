1d8e6c10f27606b50fdeb611703049db3221344e
Francisco Ricardo <78240032+franric@users.noreply.github.com>
2021-11-15 23:24:45 -0400

    Modificação da Rota Principal

--- foxcon/App_Start/RouteConfig.cs
+++ foxcon/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace foxcon
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Departamento", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
